d02801ac2c01b42da1ed74c64406f4bde89ad2a7
hisa <hsaito@TPC091>
2018-09-05 13:04:28 +0900

    もっとうざく

--- Test.java
+++ Test.java
@@ -17,7 +17,7 @@ public class Test {
      * @param message
      */
     public static void printUzai(String message){
-            for(int i=0;i<50;i++){
+            for(int i=0;i<99;i++){
                 System.out.println(i + ":" + message) ;
             }
     }
