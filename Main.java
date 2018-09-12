0e4e2b37e2b8aff085abc2210451c71b7504696e
hisa <saitou@tsn.co.jp>
2018-09-12 12:46:52 +0900

    #x

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
-//import javax.lang.model.util.ElementScanner6;
+import javax.lang.model.util.ElementScanner6;
 
 public class Main{
   public static void main(String[] args) {
     RatePlan palnA = new RatePlan("ƒvƒ‰ƒ“A",1123.30 , new TieredRateTable(0,19.62,120,26.10,300,30.12));
     RatePlan palnB = new RatePlan("ƒvƒ‰ƒ“B",1040.10 , new TieredRateTable(0,18.17,120,24.17,300,27.77));
-    double amount = 543.0;
+    double amount = 200.0;
     int d = palnA.getPrice(amount) - palnB.getPrice(amount);
     if(d<0){
       System.out.printf("%s‚ª%d‰~ˆÀ‚¢%n",palnA.getName(),-d);
